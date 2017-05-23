b58a377189a65a1146e9c492e4093ee977aec5f7
im-ch <ckdgml118@naver.com>
2017-05-23 15:55:35 +0900

    jdbc

--- FinalProject/src/jeff/store/logic/AllianceStoreLogic.java
+++ FinalProject/src/jeff/store/logic/AllianceStoreLogic.java
@@ -2,6 +2,7 @@ package jeff.store.logic;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -13,34 +14,42 @@ import jeff.store.mapper.AllianceMapper;
 @Repository
 public class AllianceStoreLogic implements AllianceStore{
 	
-	@Autowired
-	private SqlSessionTemplate sqlSession;
-	private AllianceMapper mapper = sqlSession.getMapper(AllianceMapper.class);
+//	@Autowired
+//	private SqlSessionTemplate sqlSession;
+	
+	private SqlSession session = JeffSessionFactory.getInstance().getSession();
+	
+//	private AllianceMapper mapper = sqlSession.getMapper(AllianceMapper.class);
 
 	@Override
 	public Alliance selectAlliance(String companyId) {
+		AllianceMapper mapper = session.getMapper(AllianceMapper.class);
 		Alliance alliance = mapper.selectAlliance(companyId);
 		return alliance;
 	}
 
 	@Override
 	public List<String> selectAllAlliance() {
+		AllianceMapper mapper = session.getMapper(AllianceMapper.class);
 		List<String> list = mapper.selectAllAlliance();
 		return list;
 	}
 
 	@Override
 	public void createAlliance(Alliance alliance) {
+		AllianceMapper mapper = session.getMapper(AllianceMapper.class);
 		mapper.createAlliance(alliance);
 	}
 
 	@Override
 	public void deleteAlliance(String companyId) {
+		AllianceMapper mapper = session.getMapper(AllianceMapper.class);
 		mapper.deleteAlliance(companyId);
 	}
 
 	@Override
 	public void modifyAlliance(Alliance alliance) {
+		AllianceMapper mapper = session.getMapper(AllianceMapper.class);
 		mapper.modifyAlliance(alliance);
 	}
 
