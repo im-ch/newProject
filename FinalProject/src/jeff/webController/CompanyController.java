8e86293ae9eca801bd545c26778b7cb58acdb210
YUEOJIN <eojin27@naver.com>
2017-06-03 11:24:06 +0900

    hh

--- FinalProject/src/jeff/webController/CompanyController.java
+++ FinalProject/src/jeff/webController/CompanyController.java
@@ -1,14 +1,7 @@
 package jeff.webController;
 
-<<<<<<< HEAD
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-=======
-import java.util.ArrayList;
->>>>>>> eojin
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,18 +11,12 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-<<<<<<< HEAD
-import com.google.gson.JsonObject;
-
-=======
->>>>>>> eojin
 import jeff.domain.Companies;
 import jeff.domain.Company;
 import jeff.service.CompanyService;
@@ -38,223 +25,201 @@ import jeff.service.CompanyService;
 @RequestMapping("company")
 public class CompanyController {
 
-	@Autowired
-	private CompanyService service;
+   @Autowired
+   private CompanyService service;
 
-	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public String registCompany(Company company) {
-		service.registCompany(company);
+   @RequestMapping(value = "create", method = RequestMethod.POST)
+   public String registCompany(Company company) {
+      service.registCompany(company);
 
-		return "redirect:/views/login.jsp";
-	}
+      return "redirect:/views/login.jsp";
+   }
 
-	@RequestMapping(value = "modify", method = RequestMethod.GET)
-	public String ModifyCompany(String comId, Model model) {
-		Company company = service.findCompany(comId);
-		model.addAttribute("company", company);
+   @RequestMapping(value = "modify", method = RequestMethod.GET)
+   public String ModifyCompany(String comId, Model model) {
+      Company company = service.findCompany(comId);
+      model.addAttribute("company", company);
 
-		return "/companyInfo";
-	}
+      return "/companyInfo";
+   }
 
-	@RequestMapping(value = "modify", method = RequestMethod.POST)
-	public String ModifyCompany(Company company) {
-		service.updateCompany(company);
-		return "redirect:detail?comId=" + company.getComId();
-	}
+   @RequestMapping(value = "modify", method = RequestMethod.POST)
+   public String ModifyCompany(Company company) {
+      service.updateCompany(company);
+      return "redirect:detail?comId=" + company.getComId();
+   }
 
-	@RequestMapping("remove")
-	public String RemoveCompany(HttpServletRequest req) {
+   @RequestMapping("remove")
+   public String RemoveCompany(HttpServletRequest req) {
 
-		HttpSession session = req.getSession();
-		String comId = (String) session.getAttribute("comId");
-		service.removeCompany(comId);
+      HttpSession session = req.getSession();
+      String comId = (String) session.getAttribute("comId");
+      service.removeCompany(comId);
 
-		return "redirect:/main";
-	}
+      return "redirect:/main";
+   }
 
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String loginCompany(Company company, HttpServletRequest req) {
+   @RequestMapping(value = "login", method = RequestMethod.POST)
+   public String loginCompany(Company company, HttpServletRequest req) {
 
-		Company loginedCompany = service.findCompany(company.getComId());
+      Company loginedCompany = service.findCompany(company.getComId());
 
-		if (loginedCompany != null) {
-			if (loginedCompany.getComPassword().equals(company.getComPassword())) {
-				HttpSession session = req.getSession();
-				session.setAttribute("comId", company.getComId());
-				return "redirect:/main";
-			} else {
-				HttpSession session = req.getSession(false);
-				session.invalidate();
-				return "/login";
-			}
-		} else {
-			return "/login";
-		}
-	}
+      if (loginedCompany != null) {
+         if (loginedCompany.getComPassword().equals(company.getComPassword())) {
+            HttpSession session = req.getSession();
+            session.setAttribute("comId", company.getComId());
+            return "redirect:/main";
+         } else {
+            HttpSession session = req.getSession(false);
+            session.invalidate();
+            return "/login";
+         }
+      } else {
+         return "/login";
+      }
+   }
 
-	@RequestMapping("logout")
-	public String logoutCompany(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		session.invalidate();
+   @RequestMapping("logout")
+   public String logoutCompany(HttpServletRequest req) {
+      HttpSession session = req.getSession();
+      session.invalidate();
 
-<<<<<<< HEAD
-		return "redirect:/views/main.jsp";
-=======
-		return "redirect:main";
->>>>>>> eojin
-	}
+      return "redirect:/views/main.jsp";
+   }
 
-	@RequestMapping("list")
-	public ModelAndView findAllCompany() {
-		
-		List<Company> list = service.findAllCompany();
-		ModelAndView modelAndView = new ModelAndView("companyList.jsp");
-		modelAndView.addObject("companyList", list);
-		return modelAndView;
+   @RequestMapping("list")
+   public ModelAndView findAllCompany() {
+      
+      List<Company> list = service.findAllCompany();
+      ModelAndView modelAndView = new ModelAndView("companyList.jsp");
+      modelAndView.addObject("companyList", list);
+      return modelAndView;
 
-	}
+   }
 
-	@RequestMapping("findByComId")
-	public ModelAndView findByComId(@RequestParam("comId") String comId) {
-		Company company = service.findCompany(comId);
-		ModelAndView modelAndView = new ModelAndView("/companyList");
-		modelAndView.addObject("companyList", service.findCompany(comId));
-		return modelAndView;
+   @RequestMapping("findByComId")
+   public ModelAndView findByComId(@RequestParam("comId") String comId) {
+      Company company = service.findCompany(comId);
+      ModelAndView modelAndView = new ModelAndView("/companyList");
+      modelAndView.addObject("companyList", service.findCompany(comId));
+      return modelAndView;
 
-	}
+   }
 
-	@RequestMapping("findByCategory")
-	public ModelAndView findByCategory(@RequestParam("category") String category) {
-		List<Company> list = service.findCompanyByCategory(category);
-		ModelAndView modelAndView = new ModelAndView("/companyList");
-		modelAndView.addObject("companyList", service.findCompanyByCategory(category));
-		return modelAndView;
+   @RequestMapping("findByCategory")
+   public ModelAndView findByCategory(@RequestParam("category") String category) {
+      List<Company> list = service.findCompanyByCategory(category);
+      ModelAndView modelAndView = new ModelAndView("/companyList");
+      modelAndView.addObject("companyList", service.findCompanyByCategory(category));
+      return modelAndView;
 
-	}
+   }
 
-	@RequestMapping("/findBycomName")
-	public ModelAndView findBycomName(@RequestParam("comName") String comName) {
-		Company company = service.findCompanyByName(comName);
-		ModelAndView modelAndView = new ModelAndView("companyList.jsp");
-		modelAndView.addObject("companyList", service.findCompanyByName(comName));
-		return modelAndView;
-	}
+   @RequestMapping("/findBycomName")
+   public ModelAndView findBycomName(@RequestParam("comName") String comName) {
+      Company company = service.findCompanyByName(comName);
+      ModelAndView modelAndView = new ModelAndView("companyList.jsp");
+      modelAndView.addObject("companyList", service.findCompanyByName(comName));
+      return modelAndView;
+   }
 
-	@RequestMapping("/companyDetail")
-	public ModelAndView showCompanyDetail(@RequestParam("comId") String comId) {
-		ModelAndView modelAndView = new ModelAndView("companyDetail.jsp");
-		modelAndView.addObject("company", service.findCompany(comId));
-		return modelAndView;
-	}
-	
-	@RequestMapping(value="mapList", produces="application/xml")
-<<<<<<< HEAD
-	public @ResponseBody Companies mapToXml(HttpServletRequest req){
-		List<Company> list = new ArrayList<>();
-		Companies companies = new Companies();
-//		list = service.findAllCompany();
-		
-		HttpSession session = req.getSession();
-		String location = (String)session.getAttribute("location");
-		
-		list = service.findCompanyByLocation(location);
-		
-		for(Company c : list){
-			String location2 = c.getLocation();
-			String [] lo = location2.split(";");
-=======
-	public @ResponseBody Companies mapToXml(){
-		List<Company> list = new ArrayList<>();
-		Companies companies = new Companies();
-		list = service.findAllCompany();
-		
-		for(Company c : list){
-			String location = c.getLocation();
-			String [] lo = location.split(";");
->>>>>>> eojin
-			
-			String [] lo2 = lo[1].split("\\(");
-			c.setLocation(lo2[0]);
-		}
-<<<<<<< HEAD
-		companies.setCompanies(list);
-		return companies;
-	}
-	
-	@RequestMapping(value="mapListByComName", produces="application/xml")
-	public @ResponseBody Companies mapXMLByComName(HttpServletRequest req, String comName){
-		List<Company> list = new ArrayList<>();
-		Companies companies = new Companies();
-//		list = service.findAllCompany();
-		
-		HttpSession session = req.getSession();
-		String location = (String)session.getAttribute("location");
-		HashMap<String, Object> map = new HashMap<>();
-		map.put("location", location);
-		map.put("comName", comName);
-		
-		list = service.findCompanyByLocationAndCompany(map);
-		
-		for(Company c : list){
-			String location2 = c.getLocation();
-			String [] lo = location2.split(";");
-			
-			String [] lo2 = lo[1].split("\\(");
-			c.setLocation(lo2[0]);
-		}
-		companies.setCompanies(list);
-		return companies;
-	}
-	
-	@RequestMapping("locationAndComId")
-	public String findByLocation (HttpServletRequest req, @RequestParam("comName") String comName, Model model){
-		HttpSession session = req.getSession();
-		String location = (String)session.getAttribute("location");
-		HashMap<String, Object> map = new HashMap<>();
-		map.put("comName", comName);
-		map.put("location", location);
-		
-		List<Company> listCom = service.findCompanyByLocationAndCompany(map);
-		model.addAttribute("companyList", listCom);
- 		return "/recommmendCompanyList";
-	}
-	
-	@RequestMapping(value="saveLocation", produces="application/xml")
-	public void saveLocation(HttpServletRequest req, String location){
-		HttpSession session = req.getSession();
-		session.removeAttribute("location");
-		if(location.contains("로 ")){
-			String [] lo = location.split("로 ");
-			String loo = lo[0] + "로";
-			session.setAttribute("location", loo);
-		}else if (location.contains("길 ")){
-			String [] lo = location.split("길 ");
-			String loo = lo[0] + "길";
-			session.setAttribute("location", loo);
-		}else if (location.contains("동 ")){
-			String [] lo = location.split("동 ");
-			String loo = lo[0] + "동";
-			session.setAttribute("location", loo);
-		}
-=======
-		
-		companies.setCompanies(list);
-		
-		return companies;
->>>>>>> eojin
-	}
+   @RequestMapping("/companyDetail")
+   public ModelAndView showCompanyDetail(@RequestParam("comId") String comId) {
+      ModelAndView modelAndView = new ModelAndView("companyDetail.jsp");
+      modelAndView.addObject("company", service.findCompany(comId));
+      return modelAndView;
+   }
+   
+   @RequestMapping(value="mapList", produces="application/xml")
+   public @ResponseBody Companies mapToXml(HttpServletRequest req){
+      List<Company> list = new ArrayList<>();
+      Companies companies = new Companies();
+//      list = service.findAllCompany();
+      
+      HttpSession session = req.getSession();
+      String location = (String)session.getAttribute("location");
+      
+      list = service.findCompanyByLocation(location);
+      
+      for(Company c : list){
+         String location2 = c.getLocation();
+         String [] lo = location2.split(";");
+         
+         String [] lo2 = lo[1].split("\\(");
+         c.setLocation(lo2[0]);
+      }
+      companies.setCompanies(list);
+      return companies;
+   }
+   
+   @RequestMapping(value="mapListByComName", produces="application/xml")
+   public @ResponseBody Companies mapXMLByComName(HttpServletRequest req, String comName){
+      List<Company> list = new ArrayList<>();
+      Companies companies = new Companies();
+//      list = service.findAllCompany();
+      
+      HttpSession session = req.getSession();
+      String location = (String)session.getAttribute("location");
+      HashMap<String, Object> map = new HashMap<>();
+      map.put("location", location);
+      map.put("comName", comName);
+      
+      list = service.findCompanyByLocationAndCompany(map);
+      
+      for(Company c : list){
+         String location2 = c.getLocation();
+         String [] lo = location2.split(";");
+         
+         String [] lo2 = lo[1].split("\\(");
+         c.setLocation(lo2[0]);
+      }
+      companies.setCompanies(list);
+      return companies;
+   }
+   
+   @RequestMapping("locationAndComId")
+   public String findByLocation (HttpServletRequest req, @RequestParam("comName") String comName, Model model){
+      HttpSession session = req.getSession();
+      String location = (String)session.getAttribute("location");
+      HashMap<String, Object> map = new HashMap<>();
+      map.put("comName", comName);
+      map.put("location", location);
+      
+      List<Company> listCom = service.findCompanyByLocationAndCompany(map);
+      model.addAttribute("companyList", listCom);
+       return "/recommmendCompanyList";
+   }
+   
+   @RequestMapping(value="saveLocation", produces="application/xml")
+   public void saveLocation(HttpServletRequest req, String location){
+      HttpSession session = req.getSession();
+      session.removeAttribute("location");
+      if(location.contains("로 ")){
+         String [] lo = location.split("로 ");
+         String loo = lo[0] + "로";
+         session.setAttribute("location", loo);
+      }else if (location.contains("길 ")){
+         String [] lo = location.split("길 ");
+         String loo = lo[0] + "길";
+         session.setAttribute("location", loo);
+      }else if (location.contains("동 ")){
+         String [] lo = location.split("동 ");
+         String loo = lo[0] + "동";
+         session.setAttribute("location", loo);
+      }
+   }
 
-//	String 보낼때 문자 깨지는거 -> produces="text/plain;charset=UTF-8"
-	@RequestMapping(value="loadLocation", produces="text/plain;charset=UTF-8")
-	public @ResponseBody String loadLocation(HttpServletRequest req,HttpServletResponse reqs){
-		HttpSession session = req.getSession();
-		String location = (String)session.getAttribute("location");
-		
-		if(location == null || location == ""){
-			location = "서울시 강남구 봉은사로";
-		}
-		session.setAttribute("location", location);
-		return location;
-	}
-	
+//   String 보낼때 문자 깨지는거 -> produces="text/plain;charset=UTF-8"
+   @RequestMapping(value="loadLocation", produces="text/plain;charset=UTF-8")
+   public @ResponseBody String loadLocation(HttpServletRequest req,HttpServletResponse reqs){
+      HttpSession session = req.getSession();
+      String location = (String)session.getAttribute("location");
+      
+      if(location == null || location == ""){
+         location = "서울시 강남구 봉은사로";
+      }
+      session.setAttribute("location", location);
+      return location;
+   }
+   
 }
--- FinalProject/src/jeff/webController/SalesController.java
+++ FinalProject/src/jeff/webController/SalesController.java
@@ -1,13 +1,6 @@
 package jeff.webController;
 
-<<<<<<< HEAD
-=======
-import java.sql.Date;
->>>>>>> eojin
-import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.TimeZone;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import jeff.domain.CompanySales;
 import jeff.domain.Sales;
@@ -31,85 +22,70 @@ import jeff.service.SalesService;
 @Controller
 public class SalesController {
 
-	@Autowired
-	private SalesService salesService;
+   @Autowired
+   private SalesService salesService;
 
-	@RequestMapping(value = "/register", method = { RequestMethod.GET, RequestMethod.POST })
-	public String registSales(HttpServletRequest req, Sales sales) {
-		HttpSession session = req.getSession();
+   @RequestMapping(value = "/register", method = { RequestMethod.GET, RequestMethod.POST })
+   public String registSales(HttpServletRequest req, Sales sales) {
+      HttpSession session = req.getSession();
 
-		// String comId = (String) session.getAttribute("comId");
-		String comId = "111";
-		sales.setCompanyId(comId);
-		salesService.registSales(sales);
+      String comId = (String) session.getAttribute("comId");
+      sales.setCompanyId(comId);
+      salesService.registSales(sales);
 
-		return "redirect:list";
-	}
+      return "redirect:list";
+   }
 
-	@RequestMapping(value = "/remove")
-	public String removeSales(Sales sales) {
+   @RequestMapping(value = "/remove")
+   public String removeSales(Sales sales) {
 
-		salesService.removeSales(sales.getSalesId());
+      salesService.removeSales(sales.getSalesId());
 
-		return "redirect:list";
-	}
+      return "redirect:list";
+   }
 
-	@RequestMapping(value = "/update", method = RequestMethod.GET)
-	public String updateSales(Sales sales, Model model) {
+   @RequestMapping(value = "/update", method = RequestMethod.GET)
+   public String updateSales(Sales sales, Model model) {
 
-		sales = salesService.findBySalesId(sales.getSalesId());
+      sales = salesService.findBySalesId(sales.getSalesId());
 
-		model.addAttribute("sales", sales);
+      model.addAttribute("sales", sales);
 
-		return "modifySales";
-	}
+      return "modifySales";
+   }
 
-	public String modifySales(Sales sales) {
+   public String modifySales(Sales sales) {
 
-		salesService.updateSales(sales);
+      salesService.updateSales(sales);
 
-		return "redirect:list";
-	}
+      return "redirect:list";
+   }
 
-	@RequestMapping(value = "/list")
-	public String findAllSales(HttpServletRequest req, Model model) {
+   @RequestMapping(value = "/list")
+   public String findAllSales(HttpServletRequest req, Model model) {
 
-		HttpSession session = req.getSession();
+      HttpSession session = req.getSession();
 
-<<<<<<< HEAD
-		String comId = (String) session.getAttribute("comId");
+      String comId = (String) session.getAttribute("comId");
 
-		List<Sales> list = salesService.findSalesByCompany(comId);
+      List<Sales> list = salesService.findSalesByCompany(comId);
 
-=======
-		// String comId = (String) session.getAttribute("comId");
-		String comId = "111";
+      model.addAttribute("sales", list);
 
-		List<Sales> list = salesService.findSalesByCompany(comId);
+      return "salesList";
+   }
 
-		System.out.println(list.size());
-		System.out.println(list.toString());
->>>>>>> eojin
-		model.addAttribute("sales", list);
+   @RequestMapping(value = "/calendarDetailAjax", produces = "application/json")
+   public @ResponseBody CompanySales calendarDetailAjax(HttpServletRequest request, ModelMap modelMap,
+         @ModelAttribute Sales sales) throws Exception {
+      CompanySales companySales = new CompanySales();
+      List<Sales> list = salesService.findSalesByCompany("111");
 
-		return "salesList";
-	}
-
-	@RequestMapping(value = "/calendarDetailAjax", produces = "application/json")
-	public @ResponseBody CompanySales calendarDetailAjax(HttpServletRequest request, ModelMap modelMap,
-			@ModelAttribute Sales sales) throws Exception {
-		CompanySales companySales = new CompanySales();
-		List<Sales> list = salesService.findSalesByCompany("111");
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> eojin
-		try {
-			companySales.setSalesList(list);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return companySales;
-	}
+      try {
+         companySales.setSalesList(list);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      return companySales;
+   }
 }
