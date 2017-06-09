0e30f18ff50eeb8f1499272f277a28e8dc3e36bc
im-ch <ckdgml118@naver.com>
2017-06-09 11:07:36 +0900

    before

--- FinalProject/src/jeff/webController/AllianceController.java
+++ FinalProject/src/jeff/webController/AllianceController.java
@@ -11,9 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jeff.common.exception.YzRuntimeException;
 import jeff.domain.Alliance;
+import jeff.domain.Company;
 import jeff.domain.CompanyImage;
 import jeff.domain.Coupon;
 import jeff.domain.Review;
@@ -42,61 +45,82 @@ public class AllianceController {
 	@Autowired
 	private CouponService couponService;
 
+	@RequestMapping(value = "regist", method = RequestMethod.GET)
+	public String registAlliance(Model model, HttpServletRequest req) {
+
+		HttpSession session = req.getSession();
+		String comId = (String) session.getAttribute("comId");
+		Company company = companyService.findCompany(comId);
+
+		String[] lo = company.getLocation().split(";");
+		company.setLocation("[" + lo[0] + "] " + lo[1] + " " + lo[2]);
+
+		model.addAttribute("company", company);
+
+		return "/allianceForm";
+	}
+
 	@RequestMapping(value = "regist", method = RequestMethod.POST)
 	public String registAlliance(Alliance alliance, HttpServletRequest req) {
 
 		HttpSession session = req.getSession();
 		String comId = (String) session.getAttribute("comId");
-		alliance.setCompany(companyService.findCompany(comId));
+		Company company = companyService.findCompany(comId);
+		alliance.setCompany(company);
 		service.registAlliance(alliance);
-		return "redirect:allianceDetail?comId=test";
+		System.out.println(alliance.getCompany().getComId());
+		return "redirect:detail?comId=" + comId;
 	}
 
-	@RequestMapping(value = "update", method = RequestMethod.POST)
-	public String updateAlliance(Alliance alliance) {
-		service.updateAlliance(alliance);
-		String comId = alliance.getCompany().getComId();
-		return "redirect:detail.do?comId=" + comId;
-	}
+	@RequestMapping(value = "modify", method = RequestMethod.GET)
+	public String updateAlliance(String comId, Model model) {
 
-	@RequestMapping("delete")
-	public String removeAlliance(String comId) {
-		service.removeAlliance(comId);
-		return "companyMypage";
-	}
-
-	@RequestMapping("detail")
-	public String findAlliance(String comId, Model model) {
 		Alliance alliance = service.findAlliance(comId);
 		model.addAttribute("alliance", alliance);
 
-		return "allianceDetail";
+		return "/allianceModify";
 	}
 
-	@RequestMapping("list")
-	public String findAllianceCompany(Model model, HttpServletRequest req) {
+	@RequestMapping(value = "modify", method = RequestMethod.POST)
+	public String updateAlliance(Alliance alliance, HttpServletRequest req) {
 		HttpSession session = req.getSession();
 		String comId = (String) session.getAttribute("comId");
-		List<String> list = service.findAllAlliance();
-		List<Alliance> allianceList = new ArrayList<>();
+		alliance.setCompany(companyService.findCompany(comId));
+		service.updateAlliance(alliance);
+		return "redirect:detail?comId=" + comId;
+	}
 
-		if (list != null) {
-			for (int i = 0; i < list.size(); i++) {
-				Alliance a = service.findAlliance(list.get(i));
-				allianceList.add(a);
-				continue;
-			}
-		} else {
-			YzRuntimeException ex = new YzRuntimeException("제휴제안서가 없습니다.");
-			if (comId == null) {
-				ex.setRedirectURL("/adminPage");
-			} else {
-				ex.setRedirectURL("/main");
-			}
-			throw ex;
-		}
-		model.addAttribute("allianceList", allianceList);
-		return "main";
+	@RequestMapping("delete")
+	public String removeAlliance(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		String comId = (String) session.getAttribute("comId");
+		service.removeAlliance(comId);
+
+		return "redirect:/views/main.jsp";
+	}
+
+	// 기업이 자신의 제휴제안서 볼 때 메소드
+	@RequestMapping("detail")
+	public ModelAndView allianceDetail(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		String comId = (String) session.getAttribute("comId");
+		ModelAndView modelAndView = new ModelAndView("allianceDetail");
+		Alliance alliance = service.findAlliance(comId);
+		Company company = alliance.getCompany();
+		String[] lo = company.getLocation().split(";");
+		company.setLocation("[" + lo[0] + "] " + lo[1] + " " + lo[2]);
+		alliance.setCompany(company);
+
+		modelAndView.addObject("alliance", alliance);
+		return modelAndView;
+	}
+
+	// 관리자가 기업 제휴제안서 볼 때 메소드
+	@RequestMapping("findComId")
+	public ModelAndView allianceDetail(@RequestParam("comId") String comId) {
+		ModelAndView modelAndView = new ModelAndView("allianceDetail");
+		modelAndView.addObject("alliance", service.findAlliance(comId));
+		return modelAndView;
 	}
 
 	@RequestMapping(value = "companyDetail")
@@ -107,26 +131,17 @@ public class AllianceController {
 		HttpSession session = req.getSession();
 		String userId = (String) session.getAttribute("userId");
 		List<CompanyImage> img = imgService.findCompanyImageList(comId);
-		
+
 		couponService.removeExprCoupon();
-		
+
 		List<Coupon> couponList = couponService.findCouponByCompany(comId);
-		
+
 		model.addAttribute("coupon", couponList);
 		model.addAttribute("img", img);
 		model.addAttribute("userId", userId);
 		model.addAttribute("alliance", alliance);
 		model.addAttribute("review", review);
 		return "companyDetail";
-	}
-	
-	@RequestMapping("myDetail")
-	public String findAlliance(HttpServletRequest req, Model model) {
-		HttpSession session = req.getSession();
-		String comId = (String)session.getAttribute("comId");
-		Alliance alliance = service.findAlliance(comId);
-		model.addAttribute("alliance", alliance);
-		return "/allianceDetail";
 	}
 
 }
