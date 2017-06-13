34e54b6ffb3c8661d106123606e4485fbfe0e222
heehyun <gmlgus3372@google.com>
2017-06-13 09:17:10 +0900

    zz

--- FinalProject/src/jeff/mobileController/UserCouponMobileController.java
+++ FinalProject/src/jeff/mobileController/UserCouponMobileController.java
@@ -1,8 +1,107 @@
 package jeff.mobileController;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import jeff.domain.Company;
+import jeff.domain.Coupon;
+import jeff.service.CompanyService;
+import jeff.service.CouponService;
+import jeff.service.UserCouponService;
+
+@RequestMapping(value="mobile/userCoupon")
 @Controller
 public class UserCouponMobileController {
+	@Autowired
+	private UserCouponService userCouponService;
+
+	@Autowired
+	private CouponService couponService;
+	
+	@Autowired
+	private CompanyService companyService;
+
+	@RequestMapping(value = "/register")
+	public @ResponseBody String registCoupon(HttpServletRequest req, int couponId) {
+
+		HttpSession session = req.getSession();
+
+		if (session == null || session.getAttribute("userId") == null) {
+			return "login";
+		}
+
+		String userId = (String) session.getAttribute("userId");
+		
+		List<Integer> list = userCouponService.findUserCoupons(userId);
+
+		for (int i = 0; i < list.size(); i++) {
+			if (couponId == list.get(i)) {
+				return "false";
+			}
+		}
+
+		userCouponService.registUserCoupon(userId, couponId);
+		return "true";
+
+	}
+
+	@RequestMapping(value = "/detailCoupon")
+	public ModelAndView findCouponByCouponId(int couponId) {
+		ModelAndView modelAndView = new ModelAndView("couponDetail");
+		Coupon coupon = couponService.findCoupon(couponId);
+		Company company = companyService.findCompany(coupon.getComId());
+		modelAndView.addObject("company", company);
+		modelAndView.addObject("coupon", coupon);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/myCoupons", produces = "application/xml")
+	public String findCouponByUser(HttpServletRequest req, Model model) {
+		HttpSession session = req.getSession();
+
+		if (session == null || session.getAttribute("userId") == null) {
+			return "redirect:/views/login.jsp";
+		}
+		String userId = (String) session.getAttribute("userId");
+		List<Integer> list = userCouponService.findUserCoupons(userId);
+		List<Coupon> coupons = new ArrayList<>();
+
+		for (int i = 0; i < list.size(); i++) {
+			coupons.add(couponService.findCoupon(list.get(i)));
+		}
+
+		model.addAttribute("coupon", coupons);
+
+		return "/userCouponList";
+	}
+
+	@RequestMapping(value = "/remove")
+	public @ResponseBody String removeCoupon(HttpServletRequest req, int couponId) {
+		HttpSession session = req.getSession();
+		String userId = (String) session.getAttribute("userId");
+
+		userCouponService.removeUserCoupon(userId, couponId);
+		return "remove";
+	}
+
+	@RequestMapping(value = "/use")
+	public String useCoupon(HttpServletRequest req, int couponId) {
+		HttpSession session = req.getSession();
+		String userId = (String) session.getAttribute("userId");
+		
+		userCouponService.useUserCoupon(userId, couponId);
+		
+		return "redirect:userCoupon/myCoupons";
+	}
 
 }
--- FinalProject/src/jeff/mobileController/UserMobileController.java
+++ FinalProject/src/jeff/mobileController/UserMobileController.java
@@ -1,8 +1,65 @@
 package jeff.mobileController;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import jeff.domain.Company;
+import jeff.domain.User;
+import jeff.service.CompanyService;
+import jeff.service.UserService;
+
+@RequestMapping(value = "mobile/user")
 @Controller
 public class UserMobileController {
+	@Autowired
+	private UserService service;
+
+	@Autowired
+	private CompanyService comService;
+
+	@RequestMapping(value = "login")
+	public @ResponseBody String loginUser(@RequestParam("loginId") String id,
+			@RequestParam("loginPassword") String password, HttpServletRequest req) {
+
+		User loginedUser = service.findUser(id);
+		Company loginedCompany = comService.findCompany(id);
+
+		if (loginedUser != null || loginedCompany != null) {
+			if (loginedUser != null && loginedUser.getPassword().equals(password)) {
+				HttpSession session = req.getSession();
+				session.setAttribute("userId", id);
+				return "true";
+			} else if (loginedCompany != null && loginedCompany.getComPassword().equals(password)) {
+				HttpSession session = req.getSession();
+				session.setAttribute("comId", id);
+				return "true";
+			} else {
+				HttpSession session = req.getSession(false);
+				session.invalidate();
+				return "false";
+			}
+		} else {
+			return "false";
+		}
+	}
+
+	@RequestMapping(value = "logout")
+	public @ResponseBody String logoutUser(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		session.invalidate();
+		return "redirect:/views/main.jsp";
+	}
 
 }
